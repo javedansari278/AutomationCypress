3a6346dd6e41c9653dc38ba992918adcdf64936e
javedansari278 <javedansari278@gmail.com>
2022-01-28 16:05:33 +0530

    Test data updated

---
+++ Test.js
@@ -0,0 +1,12 @@
+const fs=require('fs-extra')
+
+function DateTimestampS(){
+    var date=new Date()
+    var dateTimestamp  = date.getDate()+'_'+(date.getMonth()+1)+'_'+date.getFullYear()+'_'+date.getHours()+'hh_'+date.getMinutes()+'mm_'+date.getSeconds()+'ss_'+date.getMilliseconds()+'ms'
+    return dateTimestamp
+}
+
+var src='C:/Users/pc/Documents/Cypress_workspace/cypress/reports/index.html'
+var dest = 'C:/Users/pc/Documents/Cypress_workspace/Results/'+'index_'+DateTimestampS()+'.html'
+
+fs.moveSync(src,dest,{overwrite:true})
